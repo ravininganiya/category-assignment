74c747ca3267d2bfca14e05ac677b69e966f9eac
ravininganiya <ry4raviyadav@gmail.com>
2020-12-28 10:55:08 +0530

    Updates

---
+++ precache-manifest.cc52fae0c6e06852ab52e62417a572dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ac7f1cc5381f84434c83409b44fb17",
+    "url": "/category-assignment/index.html"
+  },
+  {
+    "revision": "889d43141086af2dfe5f",
+    "url": "/category-assignment/static/css/2.d88ce3aa.chunk.css"
+  },
+  {
+    "revision": "9300265bce0ced617444",
+    "url": "/category-assignment/static/css/main.6658e463.chunk.css"
+  },
+  {
+    "revision": "889d43141086af2dfe5f",
+    "url": "/category-assignment/static/js/2.3d288b3b.chunk.js"
+  },
+  {
+    "revision": "9300265bce0ced617444",
+    "url": "/category-assignment/static/js/main.d549dfd5.chunk.js"
+  },
+  {
+    "revision": "8801686fca60cbcd46c2",
+    "url": "/category-assignment/static/js/runtime~main.bd1ea8b4.js"
+  }
+]);
